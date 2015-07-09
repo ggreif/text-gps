5ba3551cfa559e6c6ad6cc1bf367fa79f5cacd5e
Gabor Greif <ggreif@gmail.com>
2015-07-10 00:06:37 +0200

    be frugal

--- Gps.hs
+++ Gps.hs
@@ -5,7 +5,7 @@ module Gps (encode, decode) where
 --    "\t\n\v\f\r \160\5760\8192\8193\8194\8195\8196\8197\8198\8199\8200\8201\8202\8239\8287\12288"
 -- http://en.wikipedia.org/wiki/Whitespace_character
 
-import Data.List
+import Data.List (elemIndex)
 
 encode :: String -> String
 encode = go (0 :: Int, 0 :: Int)
